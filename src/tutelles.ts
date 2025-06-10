49587939f16a372003a5a93b0711c6c5477184e0
Pascal Grange <pascal@grange.nom.fr>
2025-06-10 18:09:40 +0200

    EN COURS Construis la map siren -> tutell

--- src/tutelles.ts
+++ src/tutelles.ts
@@ -18,3 +18,130 @@ function hierarchy(services, service) {
         return [service];
     }
 }
+
+type ServiceDila = {
+    itm_identifiant: string;
+    nom: string;
+    siret: string;
+    id: string;
+    hierarchie: Array<{type_hierarchie: string, service: string}>;
+}
+
+type Service = {
+    id: string;
+    tutelles: Array<string>;
+}
+
+export function organigramme(database: Array<ServiceDila>): Array<Service> {
+    const un = passeUne(database);
+    const deux = passeDeux(un);
+    const trois = passeTrois(deux);
+    const services: Array<Service> = [];
+    for (const [_id, service] of trois) {
+        services.push({
+            ...service.donneesService,
+            id: service.id,
+            tutelles: service.ancetres
+        });
+    }
+    return services;
+}
+
+type ServicePasseUne = {
+    id: string;
+    fils: Array<string>;
+    donneesService: DonneesService;
+}
+
+type DonneesService = {
+    itm_identifiant: string;
+    siret: string;
+    nom: string;
+}
+
+function passeUne(database: Array<ServiceDila>): Map<string, ServicePasseUne> {
+    const servicesMap = new Map<string, ServicePasseUne>();
+    for (const service of database) {
+        servicesMap.set(service.id, {
+            id: service.id,
+            fils: fils(service),
+            donneesService: {
+                itm_identifiant: service.itm_identifiant,
+                siret: service.siret,
+                nom: service.nom,
+            }
+        });
+    }
+    return servicesMap;
+}
+
+function fils(service: ServiceDila): string[] {
+    const filsList: string[] = [];
+    for (const hierarchie of service.hierarchie) {
+        if (hierarchie.type_hierarchie === 'Service Fils') {
+            const serviceFils = hierarchie.service;
+            filsList.push(serviceFils);
+        }
+    }
+    return filsList;
+}
+
+type ServicePasseDeux = {
+    id: string;
+    parents: Array<string>;
+    donneesService: DonneesService;
+}
+
+function passeDeux(database: Map<string, ServicePasseUne>): Map<string, ServicePasseDeux> {
+    const servicesMap = new Map<string, ServicePasseDeux>();
+
+    for (const [_id, service] of database) {
+        servicesMap.set(service.id, {
+            id: service.id,
+            parents: [],
+            donneesService: service.donneesService
+        });
+    }
+    for (const [_id, service] of database) {
+        for (const fils of service.fils) {
+            servicesMap.get(fils)?.parents.push(service.id);
+        }
+    }
+
+    return servicesMap;
+}
+
+type ServicePasseTrois = {
+    id: string;
+    ancetres: Array<string>;
+    donneesService: DonneesService;
+}
+
+function passeTrois(database: Map<string, ServicePasseDeux>): Map<string, ServicePasseTrois> {
+    const servicesMap = new Map<string, ServicePasseTrois>();
+
+    for (const [_id, service] of database) {
+        ancetres(database, servicesMap, service);
+    }
+    return servicesMap;
+}
+
+function ancetres(databaseIn: Map<string, ServicePasseDeux>, databaseOut: Map<string, ServicePasseTrois>, service: ServicePasseDeux): string[] {
+    const serviceDeux = databaseOut.get(service.id);
+    if (serviceDeux) {
+        return serviceDeux.ancetres;
+    } else {
+        const ancetresList: string[] = [];
+        for (const parent of service.parents) {
+            ancetresList.push(parent);
+            ancetresList.push(...ancetres(databaseIn, databaseOut, databaseIn.get(parent)!));
+        }
+        databaseOut.set(service.id, {
+            id: service.id,
+            ancetres: ancetresList,
+            donneesService: service.donneesService
+        });
+        return ancetresList;
+    }
+}
+
--- tests/tutelles.spec.ts
+++ tests/tutelles.spec.ts
@@ -1,6 +1,6 @@
 import assert from 'node:assert';
 import describe, {  it } from 'node:test';
-import { tutelles } from '../src/tutelles';
+import { organigramme as calculOrganigramme, tutelles } from '../src/tutelles';
 
 import baseDila from './dila_refOrga_admin_Etat_fr_20250526.json';
 
@@ -8,9 +8,21 @@ describe('Tutelles', () => {
     it("trouve la tutelle du siret qui dépend d'un ministère", () => {
         const service = '12004701400036';
         const expected_tutelles = ['172218'];
-
+        
         assert.deepEqual(tutelles(service, baseDila['service']), expected_tutelles);
     });
+
+    it("trouve toutes les tutelles possibles du siret", () => {
+        const service = '12004701400036';
+        const expected_tutelles = ['172218'];
+
+        const organigramme = calculOrganigramme(baseDila['service']);
+
+        console.log(organigramme);
+
+        assert.deepEqual(organigramme.find(s => s.siret === service), expected_tutelles);
+    });
+
     // it("trouve la tutelle du siret qui dépend de l'arcom", () => {
     //     const service = '12004701400036'; //FIXME
     //     const expected_tutelles = ['172218']; //FIXME
@@ -18,3 +30,26 @@ describe('Tutelles', () => {
     //     assert.equal(tutelles(service, baseDila['service']), expected_tutelles);
     // });
 });
+type Service = {
+    hierarchie: Array<{type_hierarchie: string, service: string}>;
+    id: string;
+    itm_identifiant: string;
+    siret?: string;
+}
+
+function mapSiretATutelles(baseDeDonnees: Array<Service>): Map<string, string[]> {
+    const tutellesMap = new Map<string, string[]>();
+    for (const service of baseDeDonnees) {
+        for (const hierarchie of service.hierarchie) {
+            if (hierarchie.type_hierarchie === 'Service Fils') {
+                const serviceFils = baseDeDonnees.find(s => s.id === hierarchie.service);
+                if (serviceFils && serviceFils.siret) {
+                    const tutelles = tutellesMap.get(serviceFils.siret) || [];
+                    tutellesMap.set(serviceFils.siret, [...tutelles, service.itm_identifiant]);
+                }
+            }
+        }
+    }
+    return tutellesMap;
+}
+
